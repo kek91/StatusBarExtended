a6fa4089e225360a7cca0a8d69f5d010227c6a4a
eugenesvk <eugenesvk@users.noreply.github.com>
2017-08-28 16:20:09 +0300

    Moved justification constants to a class to make them easier to access

--- statusbarextended/__init__.py
+++ statusbarextended/__init__.py
@@ -8,13 +8,14 @@ import glob
 from byteconverter import ByteConverter
 #from PyQt5.QtWidgets import QApplication
 
+class Just: # Justify elements in the status bar
+    Fd = 5 # Justify Folder format: up to 9,999
+    Fl = 5 # Justify Folder format: up to 9,999
+    Sz = 7 # Justify Size format:   up to 999.0 b
+
 class StatusBarExtended(DirectoryPaneListener):
 
     def refresh(self):
-
-        justFd = 5 # Justify Folder format: up to 9,999
-        justFl = 5 # Justify Folder format: up to 9,999
-        justSz = 7 # Justify Size format:   up to 999.0 b
 
         pane = self.pane.window.get_panes().index(self.pane)
         statusbar_pane = ""
@@ -22,69 +23,68 @@ class StatusBarExtended(DirectoryPaneListener):
         pane_show_hidden_files = load_json('Panes.json')[pane]['show_hidden_files']
         pane_show_hidden_files = "◻" if pane_show_hidden_files == True else "◼"
         #alternative icons: 👁◎◉✓✗
-        current_dir = self.pane.get_path()
-        dir_folders = 0
-        dir_files = 0
-        dir_filesize = 0
+        current_dir   = self.pane.get_path()
+        dir_folders   = 0
+        dir_files     = 0
+        dir_filesize  = 0
         dir_files_in_dir = glob.glob(current_dir + "/*")
         if dir_files_in_dir:
             for f in dir_files_in_dir:
                 if path.isdir(f):
-                    dir_folders += 1
+                    dir_folders      += 1
                 else:
-                    dir_files += 1
+                    dir_files        += 1
                     try:
                         dir_filesize += stat(f).st_size
                     except Exception as e:
                         continue
 
         bc = ByteConverter(dir_filesize)
-        dir_foldersK	= str("{0:,}".format(dir_folders))	# old use str(dir_folders)
-        dir_filesK  	= str("{0:,}".format(dir_files))  	# ' ' instead of ',' .replace(',', ' ')
+        dir_foldersK   = str("{0:,}".format(dir_folders))  # old use str(dir_folders)
+        dir_filesK     = str("{0:,}".format(dir_files))    # ' ' instead of ',' .replace(',', ' ')
         if(self.pane == self.pane.window.get_panes()[0]):
-            statusbar_pane += "Pane: Left  "
+            statusbar_pane      += "Pane: Left  "
         else:
-            statusbar_pane += "Pane: Right "
-        statusbar_pane += ""	+ pane_show_hidden_files	+ "    "
+            statusbar_pane      += "Pane: Right "
+        statusbar_pane          += ""        + pane_show_hidden_files             + "    "
         if dir_folders == 0:
-            statusbar_pane += "      "  + ''.rjust(justFd, ' ')    + "    "
+            statusbar_pane      += "      "  + ''.rjust(Just.Fd, ' ')             + "    "
         elif dir_folders > 9999:
-            statusbar_pane += "Dirs: "  + dir_foldersK.rjust(justFl, ' ')    + "   "
+            statusbar_pane      += "Dirs: "  + dir_foldersK.rjust(Just.Fl, ' ')   + "   "
         else:
-            statusbar_pane += "Dirs: "  + dir_foldersK.rjust(justFl, ' ')    + "    "
+            statusbar_pane      += "Dirs: "  + dir_foldersK.rjust(Just.Fl, ' ')   + "    "
         if dir_files > 0:
             if dir_files > 9999:
-                statusbar_pane += "Files: "	+ dir_filesK.rjust(justFd, ' ')	+ "   "
+                statusbar_pane  += "Files: " + dir_filesK.rjust(Just.Fd, ' ')     + "   "
             else:
-                statusbar_pane += "Files: " + dir_filesK.rjust(justFd, ' ')      + "    "
+                statusbar_pane  += "Files: " + dir_filesK.rjust(Just.Fd, ' ')     + "    "
         else:
-            statusbar_pane += "       " + ''.rjust(justFl, ' ')      + "    "
-        statusbar_pane += "Size: "  + str(bc.calc()).rjust(justSz, ' ')  + "    "
+            statusbar_pane      += "       " + ''.rjust(Just.Fl, ' ')             + "    "
+        statusbar_pane          += "Size: "  + str(bc.calc()).rjust(Just.Sz, ' ') + "    "
 
         show_status_message(statusbar_pane, 5000)
 
-
     def show_selected_files(self):
-        selected    	= self.pane.get_selected_files()
-        dir_folders 	= 0
-        dir_files   	= 0
-        dir_filesize	= 0
+        selected        = self.pane.get_selected_files()
+        dir_folders     = 0
+        dir_files       = 0
+        dir_filesize    = 0
 
         if selected:
             for f in selected:
                 if path.isdir(f):
-                    dir_folders += 1
+                    dir_folders     += 1
                 else:
-                    dir_files   	+= 1
-                    dir_filesize	+= stat(f).st_size
+                    dir_files       += 1
+                    dir_filesize    += stat(f).st_size
 
             bc = ByteConverter(dir_filesize)
-            dir_foldersK    = str("{0:,}".format(dir_folders))  # old use str(dir_folders)
-            dir_filesK      = str("{0:,}".format(dir_files))    # ' ' instead of ',' .replace(',', ' ')
-            statusbar = "Selected*   "
-            statusbar += "Dirs: "  + dir_foldersK.rjust(justFd, ' ')    + "   "
-            statusbar += "Files: " + dir_filesK.rjust(justFl, ' ')      + "  "
-            statusbar += "∑ Size: " + str(bc.calc()).rjust(justSz, ' ')
+            dir_foldersK   = str("{0:,}".format(dir_folders)) # old use str(dir_folders)
+            dir_filesK     = str("{0:,}".format(dir_files))   # for ' ' instead of ',' .replace(',', ' ')
+            statusbar      = "Selected*   "
+            statusbar     += "Dirs: "   + dir_foldersK.rjust(Just.Fd, ' ')    + "   "
+            statusbar     += "Files: "  + dir_filesK.rjust(Just.Fl, ' ')      + "  "
+            statusbar     += "∑ Size: " + str(bc.calc()).rjust(Just.Sz, ' ')
             show_status_message(statusbar)
 
         else:
