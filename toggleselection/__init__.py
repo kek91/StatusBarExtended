28686b35a22528dc6f4ce5c440bad67710877318
eugenesvk <eugenesvk@users.noreply.github.com>
2021-08-06 14:18:41 +0700

    :beetle: update status bar when hidden files' visibility is toggled

--- toggleselection/__init__.py
+++ toggleselection/__init__.py
@@ -26,6 +26,36 @@ class _CorePaneCommand(DirectoryPaneCommand): # copy from core/commands/__init__
     def move_cursor_end(      self,     toggle_selection=False):
         self.pane.move_cursor_end(      toggle_selection)
 
+    def toggle_hidden_files(self):
+        _toggle_hidden_files(self.pane, not _is_showing_hidden_files(self.pane))
+def _toggle_hidden_files(pane, value):
+    if value:
+        pane._remove_filter(_hidden_file_filter)
+    else:
+        pane._add_filter(_hidden_file_filter)
+    _get_pane_info(pane)['show_hidden_files'] = value
+    save_json('Panes.json')
+def _is_showing_hidden_files(pane):
+    return _get_pane_info(pane)['show_hidden_files']
+def _get_pane_info(pane):
+    settings = load_json('Panes.json', default=[])
+    default = {'show_hidden_files': False}
+    pane_index = pane.window.get_panes().index(pane)
+    for _ in range(pane_index - len(settings) + 1):
+        settings.append(default.copy())
+    return settings[pane_index]
+def _hidden_file_filter(url):
+    if PLATFORM == 'Mac' and url == 'file:///Volumes':
+        return True
+    scheme, path = splitscheme(url)
+    return scheme != 'file://' or not is_hidden(path)
+
+def _get_opposite_pane(pane):
+    panes = pane.window.get_panes()
+    return panes[(panes.index(pane) + 1) % len(panes)]
+
+
+
 class CommandEmpty(): # to avoid duplicate command execution (and "return '', args" hangs)
     def __call__(self):
         pass
@@ -35,7 +65,8 @@ class SelectionOverride(DirectoryPaneListener):
             self.show_selected_files()
             return 'command_empty', args
         elif command_name in (
-            'select_all', 'deselect'):
+            'select_all', 'deselect',
+            'toggle_hidden_files'):
             getattr(_CorePaneCommand, command_name)(self)
             self.show_selected_files()
             return 'command_empty', args
