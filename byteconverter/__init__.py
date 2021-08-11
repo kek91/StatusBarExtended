1c1b512b34b4c3f86fcc390d428566d4e47e683f
eugenesvk <eugenesvk@users.noreply.github.com>
2021-08-11 23:35:03 +0700

    feat: make ByteConverter use SizeDivisor

--- byteconverter/__init__.py
+++ byteconverter/__init__.py
@@ -1,10 +1,18 @@
 # Utility class for converting bytes
 
+import statusbarextended_config as SBEcfg
+
 class ByteConverter():
     def __init__(self, n):
         self.bytes = n
+        cfg = SBEcfg.SingletonConfig()
+        cfgCurrent, exit_status = cfg.loadConfig()
+        if cfgCurrent is None:
+            self.sizeDivisor = cfg.Default['SizeDivisor']
+        else:
+            self.sizeDivisor =  cfgCurrent['SizeDivisor']
     def calc(self):
         for x in ['b', 'k', 'M', 'G', 'T']:
-            if self.bytes < 1024.0:
+            if self.bytes < self.sizeDivisor:
                 return "%3.1f %s" % (self.bytes, x) if x!='b' else "%5.0f" % (self.bytes)
-            self.bytes /= 1024.0
+            self.bytes   /= self.sizeDivisor
